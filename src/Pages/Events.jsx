1e9e64a3890f890ae789c0a87ea3804747eb3a6d
Tanmay0215 <tanmaygupta.0215@gmail.com>
2024-09-28 14:02:03 +0530

    sample update

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -40,7 +40,7 @@ const Events = ()=>{
                 <div className="bg-black w-screen h-screen overflow-x-hidden">
             <MainSlider data = {data}/>
             <div className="mt-12">
-                <div className="text-white text-2xl mx-10">Upcoming Events</div>
+                <div className="text-white text-2xl mx-10">Upcoming-Events</div>
             </div>
             <div className="mt-10 mb-10">
                 <UpcomingSlider data = {data}/>
